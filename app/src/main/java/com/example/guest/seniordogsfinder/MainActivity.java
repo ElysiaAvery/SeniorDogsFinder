2147e83d30554ab1967ad433eb753fed64e3d95d
Elysia Nason <elysia.avery@gmail.com>
2016-11-18 09:22:38 -0800

    add current app after failing to include a nav drawer

--- app/src/main/java/com/example/guest/seniordogsfinder/MainActivity.java
+++ app/src/main/java/com/example/guest/seniordogsfinder/MainActivity.java
@@ -1,22 +1,33 @@
 package com.example.guest.seniordogsfinder;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth auth;
+    @Bind(R.id.header) TextView mHeader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         auth = FirebaseAuth.getInstance();
+        Typeface goodDogFont = Typeface.createFromAsset(getAssets(), "fonts/GoodDog.otf");
+        mHeader.setTypeface(goodDogFont);
         FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
