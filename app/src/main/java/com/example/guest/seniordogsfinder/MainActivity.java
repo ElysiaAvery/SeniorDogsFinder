864ef19f896a65ea0ef005c103001f4345b4662d
Elysia Nason <elysia.avery@gmail.com>
2016-11-18 08:09:33 -0800

    add login functionality

--- app/src/main/java/com/example/guest/seniordogsfinder/MainActivity.java
+++ app/src/main/java/com/example/guest/seniordogsfinder/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.guest.seniordogsfinder;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+        FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                }
+            }
+        };
+        if (auth.getCurrentUser() != null) {
+            // User is logged in
+        }
+
     }
 }
