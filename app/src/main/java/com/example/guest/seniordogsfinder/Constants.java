fa6d4804764b32da3bae1a82f176ce31880be59f
Elysia Nason <elysia.avery@gmail.com>
2016-12-02 07:48:43 -0800

    add first commit with api key and secret

---
+++ app/src/main/java/com/example/guest/seniordogsfinder/Constants.java
@@ -0,0 +1,9 @@
+package com.example.guest.seniordogsfinder;
+
+/**
+ * Created by Guest on 12/2/16.
+ */
+public class Constants {
+    public static final String PET_KEY = BuildConfig.PET_KEY;
+    public static final String PET_TOKEN = BuildConfig.PET_TOKEN;
+}
