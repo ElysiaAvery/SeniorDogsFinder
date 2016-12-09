43896ea7dcf543ee8563bcd01ea3bf356ffd2bc9
Elysia Nason <elysia.avery@gmail.com>
2016-12-09 13:53:16 -0800

    Add finished Firebase Sponsored Dog View Holder class

--- app/src/main/java/com/example/guest/seniordogsfinder/adapters/FirebaseSponsoredDogViewHolder.java
+++ app/src/main/java/com/example/guest/seniordogsfinder/adapters/FirebaseSponsoredDogViewHolder.java
@@ -1,21 +1,33 @@
 package com.example.guest.seniordogsfinder.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.guest.seniordogsfinder.Constants;
 import com.example.guest.seniordogsfinder.R;
 import com.example.guest.seniordogsfinder.models.Dog;
+import com.example.guest.seniordogsfinder.ui.DogDetailActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 
 /**
  * Created by Guest on 12/9/16.
  */
-public class FirebaseSponsoredDogViewHolder extends RecyclerView implements View.OnClickListener {
+public class FirebaseSponsoredDogViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     private static final int MAX_WIDTH = 200;
     private static final int MAX_HEIGHT = 200;
 
@@ -48,5 +60,31 @@ public class FirebaseSponsoredDogViewHolder extends RecyclerView implements View
         String dogBreeds = toBeReplaced.replace("]", "");
         breedsTextView.setText(dogBreeds);
         addressTextView.setText(dog.getCity() + ", " + dog.getState() + " " + dog.getZip());
+    }
+
+    @Override
+    public void onClick(View view) {
+        final ArrayList<Dog> dogs = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_SPONSORED_DOG);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    dogs.add(snapshot.getValue(Dog.class));
+                }
+
+                int itemPosition = getLayoutPosition();
+
+                Intent intent = new Intent(mContext, DogDetailActivity.class);
+                intent.putExtra("position", itemPosition + "");
+                intent.putExtra("dogs", Parcels.wrap(dogs));
+
+                mContext.startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 }
